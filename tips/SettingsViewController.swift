53be2b28dc5c1401be32f62fe1c64a3b64425473
Harpreet Singh <singhh17@uw.edu>
2015-12-28 00:41:48 -0800

    Completed app

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,104 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Harpreet Singh on 12/27/15.
+//  Copyright © 2015 Harpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBOutlet var sliderOne: UISlider!
+    @IBOutlet var sliderTwo: UISlider!
+    @IBOutlet var sliderThree: UISlider!
+    @IBOutlet var oneValue: UILabel!
+    @IBOutlet var twoValue: UILabel!
+    @IBOutlet var threeValue: UILabel!
+    @IBOutlet var tipDefault: UISegmentedControl!
+    
+    
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let option1 = defaults.integerForKey("op1")
+        let option2 = defaults.integerForKey("op2")
+        let option3 = defaults.integerForKey("op3")
+        tipDefault.selectedSegmentIndex = defaults.integerForKey("tipDefault")
+
+        if(option1 != 0) {
+            sliderOne.value = Float(option1)
+            oneValue.text = String(option1)
+            tipDefault.setTitle("\(option1)%", forSegmentAtIndex: 0)
+        } else {
+            sliderOne.value = 18.0
+            oneValue.text = "18"
+            
+        }
+        
+        if(option2 != 0) {
+            sliderTwo.value = Float(option2)
+            twoValue.text = String(option2)
+            tipDefault.setTitle("\(option2)%", forSegmentAtIndex: 1)
+        } else {
+            sliderTwo.value = 20.0
+            twoValue.text = "20"
+        }
+        
+        if(option3 != 0) {
+            sliderThree.value = Float(option3)
+            threeValue.text = String(option3)
+            tipDefault.setTitle("\(option3)%", forSegmentAtIndex: 2)
+        } else {
+            sliderThree.value = 22.0
+            threeValue.text = "22"
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func oneChanged(sender: AnyObject) {
+        let value = Int(sliderOne.value)
+        oneValue.text = String(value)
+        tipDefault.setTitle("\(value)%", forSegmentAtIndex: 0)
+        defaults.setInteger(value, forKey: "op1")
+        defaults.synchronize()
+        
+    }
+    
+    @IBAction func twoChanged(sender: AnyObject) {
+        let value = Int(sliderTwo.value)
+        twoValue.text = String(value)
+        tipDefault.setTitle("\(value)%", forSegmentAtIndex: 1)
+        defaults.setInteger(value, forKey: "op2")
+        defaults.synchronize()
+    }
+    
+    @IBAction func threeChanged(sender: AnyObject) {
+        let value = Int(sliderThree.value)
+        threeValue.text = String(value)
+        tipDefault.setTitle("\(value)%", forSegmentAtIndex: 2)
+        defaults.setInteger(value, forKey: "op3")
+        defaults.synchronize()
+    }
+    
+    @IBAction func tipDefaultChanged(sender: AnyObject) {
+        defaults.setInteger(tipDefault.selectedSegmentIndex, forKey: "tipDefault")
+        defaults.synchronize()
+    }
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipControl: UISegmentedControl!
+    
+    var tipPercentages = [0.18, 0.2, 0.22]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+     
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        let tipValue1 = defaults.integerForKey("op1")
+        let tipValue2 = defaults.integerForKey("op2")
+        let tipValue3 = defaults.integerForKey("op3")
+        tipControl.selectedSegmentIndex = defaults.integerForKey("tipDefault")
+
+        
+        if (tipValue1 != 0) {
+            tipControl.setTitle("\(tipValue1)%", forSegmentAtIndex: 0)
+            tipPercentages[0] = Double(tipValue1) / 100.0
+        }
+        
+        if (tipValue2 != 0) {
+            tipControl.setTitle("\(tipValue2)%", forSegmentAtIndex: 1)
+            tipPercentages[1] = Double(tipValue2) / 100.0
+        }
+        
+        if (tipValue3 != 0) {
+            tipControl.setTitle("\(tipValue3)%", forSegmentAtIndex: 2)
+            tipPercentages[2] = Double(tipValue3) / 100.0
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
 
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
